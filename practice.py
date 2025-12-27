03f34945790d2685938bfb82bfa35037470380c0
umarmohammadraja-ux <umar.mohammadraja@gmail.com>
2025-12-27 12:07:08 -0600

    practice if statements

---
+++ practice.py
@@ -0,0 +1,19 @@
+# Input is a decimal number
+# Output is a char
+# if the grade is 90 or above then the letter should be an A
+# if the grade is 80 and below 89 then the letter should be B
+# if the grade is 70 and below 79 then the letter should be C
+# if the grade is 60 and below 69 then the letter should be D
+# if the grade is 0 and below 59 then the letter should be F
+
+Grade = input("Enter a grade to calculate: ")
+if Grade >= 90:
+    print("you have sucseeded to CHINA TOWN")
+if Grade >= 80 and Grade <= 89:
+    print("you no go to china town")
+if Grade >= 70 and Grade <= 79:
+    print("WHY YOU FAAIIILLIINGG")
+if Grade >= 60 and Grade <= 69:
+    print("get ready for de wooping")
+if Grade >=0 and Grade <= 59:
+    print("My son so dumb IQ21 he try to make friend but they call him CHINGCHONG")
